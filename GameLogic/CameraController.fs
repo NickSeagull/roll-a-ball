3bcfaddeab3f925d5be38b5004ede1fc24842b69
Nikita Tchayka <Nikita Tchayka>
2016-01-06 02:15:20 +0000

    Finish the scene and the camera

---
+++ GameLogic/CameraController.fs
@@ -0,0 +1,21 @@
+﻿namespace Core
+
+open UnityEngine
+
+type CameraController () =
+    inherit MonoBehaviour ()
+
+
+    [<SerializeField>]
+    let mutable player: GameObject = null
+
+
+    member val offset = Vector3.one with get, set
+
+
+    member this.Start () = 
+        this.offset <- this.transform.position - player.transform.position
+
+
+    member this.LateUpdate () =
+        this.transform.position <- player.transform.position + this.offset
--- GameLogic/Core.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace Core
-
-open UnityEngine
-
-type HelloWorldComponent() = 
-    inherit MonoBehaviour()
-    
-    member this.Start() = Debug.Log("Hello from F#")
